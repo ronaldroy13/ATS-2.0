506147dbf5df30da7d52a4923d71fa3c24a04fb3
Daniel <danielrferriss@gmail.com>
2018-01-28 14:51:52 -0600

    fixed some styling issues xy

--- static/navbar.js
+++ static/navbar.js
@@ -1,6 +1,6 @@
 document.getElementById("navMenu").innerHTML = 
 '<div class ="navbar">'+
-'<a href="/"> <div> <p> Home </p> </div> </a>'+
+'<a href="/"> <div> Home </div> </a>'+
 '<a href="calculator"> <div> Calculator </div> </a>' +
 '<a href="data"> <div> Data </div> </a>' +
 '</div>';
